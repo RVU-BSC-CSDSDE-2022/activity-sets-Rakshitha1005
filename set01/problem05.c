273c6fd03d4b61cff033394a4d97eefc6c20ad4f
Rakshitha Mohan <115693685+Rakshitha1005@users.noreply.github.com>
2022-12-22 09:19:42 +0000

    problem done

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,41 +1,41 @@
 #include<stdio.h>
 int input();
-int compare(int a, int b, int c);
-void output(int a, int b, int c, int largest);
+int compare(int a,int b,int c);
+void output(int a,int b,int c,int largest);
 int main()
 {
-   int a,b,c,largest;
-  a=input();
-  b=input();
-  c=input();
-  largest=compare(a,b,c);
-  output( a,  b,  c,  largest); 
+int a,b,c,largest;
+a=input();
+b=input();
+c=input();
+largest=compare(a,b,c);
+output(a,b,c,largest);
 }
 int input()
 {
- int n;
-  printf("Enter the number:");
-  scanf("%d",&n);
-  return n;
+int n;
+printf("Enter 3 values:\n");
+scanf("%d",&n);
+return n;
 }
-int compare(int a, int b, int c)
+int compare(int a,int b,int c )
 {
- int largest=0;
-  if ((a>b)&&(a>c))
-  {
-    largest=a;
-  }
-  else if ((b>a)&&(b>c))
-  {
-    largest=b;
-  }
-  else
-  {
-    largest=c;
-  }
-   return largest; 
-  }
-void output(int a, int b, int c, int largest)
+int largest=0;
+if(a>b && a>c)
 {
-  printf("the largest of %d,%d and %d is %d",a,b,c,largest);
+largest=a;
+}
+else if(b>a && b>c)
+{
+largest=b;
+}
+else
+{
+largest=c;
+}
+return largest;
+}
+void output(int a,int b,int c,int largest)
+{
+printf("The largest of %d ,%d and %d is %d\n",a,b,c,largest);
 }
