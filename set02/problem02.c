fb8cf9d2416cefd9c84edf61d75ab87c59e131b5
Rakshitha Mohan <115693685+Rakshitha1005@users.noreply.github.com>
2022-12-27 12:11:01 +0000

    problem done

--- set02/problem02.c
+++ set02/problem02.c
@@ -2,26 +2,26 @@
 int input_side();
 int check_scalene(int a, int b, int c);
 void output(int a, int b, int c, int isscalene);
-
 int main()
 {
-  int a,b,c;
+  int a,b,c,isscalene;
   a=input_side();
   b=input_side();
   c=input_side();
-  isscalene=check_scalene(a,b,c);
+  isscalene=check_scalene( a,  b,  c);
   output(a,b,c,isscalene);
-  }
+}
 int input_side()
 {
   int n;
-  printf("enter the values:\n");
+  printf("enter the value of:");
   scanf("%d",&n);
   return n;
-  }
-int check_scalene(int a,int b,int c);
+}
+int check_scalene(int a, int b, int c)
 {
-  if(a!=b && b!=c && c!=a)
+  
+  if((a!=b) &&(b!=c)  && (c!=a))
   {
     return 1;
   }
@@ -29,15 +29,18 @@ int check_scalene(int a,int b,int c);
   {
     return 0;
   }
-  }
-  void output(int a,int b,int c, int isscalene)  
-  {
-    if (isscalene==1)
-    {
-      printf("The triangle with sides %d ,%d and %d is  scalene",a,b,c,isscalene);
-      }
-    else
-    {
-      printf("The triangle with sides %d ,%d and %d is not scalene",a,b,c,isscalene);
-  }
+  
 }
+void output(int a, int b, int c, int isscalene)
+{
+  if(isscalene==1) 
+
+  
+  {
+    printf("The triangle with sides %d, %d and %d is  scalene",a,b,c);
+    }
+  else
+  {
+   printf("The triangle with sides %d,%d and %d is not scalene",a,b,c) ;
+  }
+  }
---
+++ set02/problem04.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+int input_array_size()
+{
+  int n;
+  printf("Enter the numbers :");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n, int a[n]);
+{
+  int a[100];
+  
+}
