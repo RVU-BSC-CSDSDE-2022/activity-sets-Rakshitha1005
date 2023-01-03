3a84d4228dff67b702b85d593d1830541a1a3df1
Rakshitha Mohan <115693685+Rakshitha1005@users.noreply.github.com>
2023-01-03 05:07:11 +0000

    problem done

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,17 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 int input_array_size();
 void input_array(int n, int a[n]);
 int sum_composite_numbers(int n, int a[n]);
 void output(int sum);
+int main()
+{
+  int n,a[n],sum;
+  n=input_array_size();
+  a[n]=input_array_size();
+  input_array( n, a[n]);
+  sum_composite_numbers( n,  a[n]);
+  output(n,a[n],sum);
+}
 int input_array_size()
 {
-  int n;
-  printf("Enter the numbers :");
+  printf("Input the array:");
   scanf("%d",&n);
   return n;
 }
-void input_array(int n, int a[n]);
+void input_array(int n, int a[n])
 {
-  int a[100];
-  
+  int i;
+  printf("input the array %d:\n");
+  for(i=0;i<n;i++)
+    {
+      printf("enter the number:");
+      scanf("%d",&a[n]);
+    }
+}
+void output(int sum);
+{
+  printf("The sum of composite number %d and %d is %d",n,a[n],sum)
 }
